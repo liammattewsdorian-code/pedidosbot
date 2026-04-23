e897686b17deb99629ed2eb630ad18077a5bafe4
liamhash <liammattewsdorian@gmail.com>
2026-04-22 23:20:30 -0400

    upgrade

--- page.tsx
+++ page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
